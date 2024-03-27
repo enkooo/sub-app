83f3ab5d30ef67151717a60d2fba4530ebe454bf
enko <pytlos1337@gmail.com>
2024-03-27 21:14:56 +0100

    Setup app and add base clerk auth

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
+import {
+  Ionicons,
+  FontAwesome5,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from '@expo/vector-icons'
+import { StatusBar } from 'react-native'
+
+const Layout = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors.primary,
+          tabBarLabelStyle: { fontFamily: 'mon-sb' },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="analysis"
+          options={{
+            tabBarLabel: 'Analysis',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="analytics" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="chat"
+          options={{
+            tabBarLabel: 'Chat',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="message-outline"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarLabel: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="person-circle-outline"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  )
+}
+
+export default Layout
---
+++ constants/Colors.ts
@@ -0,0 +1,5 @@
+export default {
+  primary: '#3856ff',
+  grey: '#5E5D5E',
+  dark: '#1A1A1A',
+}
