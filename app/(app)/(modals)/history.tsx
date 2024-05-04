3bf8b73d7492e43c71dc9283f2704d7c91aac70f
enko <pytlos1337@gmail.com>
2024-05-04 19:00:03 +0200

    Refactor History component to include StatusBar and improve UI

--- app/(app)/(modals)/history.tsx
+++ app/(app)/(modals)/history.tsx
@@ -10,6 +10,7 @@ import { ChatHistoryItem as ChatHistoryItemType } from '@/types/ChatHistoryItem'
 import { useRouter } from 'expo-router'
 import { getUserConversations } from '@/api/apis/getUserConversations'
 import { deleteConversation } from '@/api/apis/deleteConversation'
+import { StatusBar } from 'expo-status-bar'
 
 const History = () => {
   const [chatHistory, setChatHistory] = useState<ChatHistoryItemType[] | null>()
@@ -49,42 +50,45 @@ const History = () => {
   }, [])
 
   return (
-    <View className="flex-1 bg-gray-50">
-      <GestureHandlerRootView className="flex-1">
-        <FlatList
-          contentContainerStyle={{
-            marginTop: 10,
-            paddingBottom: 20,
-          }}
-          data={chatHistory}
-          renderItem={({ item }) => (
-            <Pressable
-              onPress={() => {
-                router.back()
-                router.push(`/chat/${item.id}`)
-              }}
-              key={item.id}
-            >
-              <ChatHistoryItem item={item} onRemove={onRemove} />
-            </Pressable>
-          )}
-          ListEmptyComponent={
-            refreshing ? null : (
-              <Text style={{ textAlign: 'center', marginTop: 20 }}>
-                No history found!
-              </Text>
-            )
-          }
-          refreshControl={
-            <RefreshControl
-              refreshing={refreshing}
-              onRefresh={getUserChatHistory}
-              tintColor="gray"
-            />
-          }
-        />
-      </GestureHandlerRootView>
-    </View>
+    <>
+      <StatusBar style="light" />
+      <View className="flex-1 bg-gray-50">
+        <GestureHandlerRootView className="flex-1">
+          <FlatList
+            contentContainerStyle={{
+              marginTop: 10,
+              paddingBottom: 20,
+            }}
+            data={chatHistory}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() => {
+                  router.back()
+                  router.push(`/chat/${item.id}`)
+                }}
+                key={item.id}
+              >
+                <ChatHistoryItem item={item} onRemove={onRemove} />
+              </Pressable>
+            )}
+            ListEmptyComponent={
+              refreshing ? null : (
+                <Text style={{ textAlign: 'center', marginTop: 20 }}>
+                  No history found!
+                </Text>
+              )
+            }
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={getUserChatHistory}
+                tintColor="gray"
+              />
+            }
+          />
+        </GestureHandlerRootView>
+      </View>
+    </>
   )
 }
 
